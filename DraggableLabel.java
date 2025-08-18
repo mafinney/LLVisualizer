d7a3a55cacba7217bc68f49e872795fe8e3dcfa1
mafinney <matthewafinney13@gmail.com>
2025-08-18 13:53:27 -0700

    add TODO for DraggableLabel

--- DraggableLabel.java
+++ DraggableLabel.java
@@ -2,6 +2,13 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+/**
+ * TODO:
+ * Redo DraggableLabel as a "dual class" including the JLabel attributes and
+ * a Node object built in. Include a method to draw a line from the object to 
+ * the object it points to as next
+ */
+
 class DraggableLabel extends JLabel {
     private Point mouseActionLocation;
 
@@ -10,12 +17,14 @@ class DraggableLabel extends JLabel {
         this.setBounds(50, 50, 100, 30);
         this.setBorder(BorderFactory.createLineBorder(color));
         addMouseListener(new MouseAdapter() {
+            @Override
             public void mousePressed(MouseEvent e) {
                 mouseActionLocation = e.getPoint();
             }
         });
 
         addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
             public void mouseDragged(MouseEvent e) {
                 if (mouseActionLocation != null) {
                     Point currLocation = getLocation();
