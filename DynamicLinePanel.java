78c4612f3c8efd7443fb76374d118714595afba7
Matthew Finney <finneym3@wwu.edu>
2025-08-20 17:31:08 -0700

    Rewrote DynamicLinePanel cleaner, need to fix error on run

--- DynamicLinePanel.java
+++ DynamicLinePanel.java
@@ -11,36 +11,23 @@ import java.awt.event.*;
 import java.util.*;
 
 class DynamicLinePanel extends JPanel {
-    ArrayList<DraggableNode> nodes;
+    DraggableNode head;
     public DynamicLinePanel(DraggableNode head) {
         super();
-        nodes = new ArrayList<>();
-        nodes.add(head);
+        this.head = head;
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        updateNodeList();
-
-        DraggableNode curr = nodes.get(1);
+        DraggableNode curr = head.getNext();
         DraggableNode next = curr.getNext();
 
-        for (int i = 3; next != null; i++) {
+        while (next != null) {
             g.drawLine(curr.getLocation().x, curr.getLocation().y, next.getLocation().x, next.getLocation().y);
             curr = next;
-            next = nodes.get(i);
+            next = next.getNext();
         }
         repaint();
-    }
-
-    private void updateNodeList() {
-        DraggableNode head = nodes.get(0);
-        DraggableNode curr = head.getNext();
-        for (int i = 1; curr != null; i++) {
-            nodes.add(i, curr);
-            curr = curr.getNext();
-        }
-        nodes.add(null);
     }
 }
